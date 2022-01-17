ad442c446c18b5b485920de213f5d648a19e489c
Krish Bhatia <krishbht15>
2022-01-18 00:57:10 +0530

    17 Jan 2022

---
+++ src/com/company/intro/Main.java
@@ -0,0 +1,33 @@
+package com.company.intro;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hey! Newton School");
+//        System.out.println("\"");
+//        System.out.println("a");
+//        System.out.print("");
+//        System.out.print("b");
+//        System.out.println("c");
+//        System.out.println(5 + 5);
+//        System.out.println(5 - 5);
+//        System.out.println(5 / 5);
+//        System.out.println(5 * 5);
+//        System.out.println(23 % 5);
+//        System.out.println("Table of 2:");
+        System.out.println("2 * 1 = " + 2 * 1 +" calculated");
+//        System.out.println("2 * 2 = " + 2 * 2);
+//        System.out.println("2 * 3 = " + 2 * 3);
+//        System.out.println("2 * 4 = " + 2 * 4);
+//        System.out.println("2 * 5 = " + 2 * 5);
+//        System.out.println("2 * 6 = " + 2 * 6);
+//        System.out.println("2 * 7 = " + 2 * 7);
+//        System.out.println("2 * 8 = " + 2 * 8);
+//        System.out.println("2 * 9 = " + 2 * 9);
+//        System.out.println("2 * 10 = " + 2 * 10);
+//        System.out.println(10 == 11);
+//        System.out.println(10 == 10);
+        System.out.println(10 <= 11);
+        System.out.println(10 > 11);
+
+    }
+}
