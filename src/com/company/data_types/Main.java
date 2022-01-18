8e06094de5189dd81bc5b6243a788ae1cf91f9a1
Krish Bhatia <krishbht15>
2022-01-18 23:38:52 +0530

    18th Jan 2022

---
+++ src/com/company/data_types/Main.java
@@ -0,0 +1,40 @@
+package com.newton_school.data_types;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println();
+        String st = s.nextLine();
+
+//        float f = s.nextFloat();
+//        String st = s.next();
+
+        System.out.println("a is equals to " + a);
+//        System.out.println("f is equals to " + f);
+        System.out.println("st is equals to " + st);
+
+//        System.out.println("This class got selected");
+//        byte b = 10;
+//        short s = 90;
+//        int a = 5;
+//        long l = 74983728749832987l;
+//        float f = 1.54f;
+//        double d = 22.5;
+//        char c='f';
+//        String st="hello";
+//        boolean bool=false;
+//        System.out.println(b);
+//        System.out.println(a);
+//        System.out.println(s);
+//        System.out.println(l);
+//        System.out.println(f);
+//        System.out.println(d);
+//        System.out.println(c);
+//        System.out.println(st);
+//        System.out.println(bool);
+    }
+}
