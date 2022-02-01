a3c042dd96ca04352650c1fdd4d93b34e24ca81d
Krish Bhatia <krishbht15>
2022-02-01 22:51:23 +0530

    1 Feb

--- src/com/company/time_n_space/Main.java
+++ src/com/company/time_n_space/Main.java
@@ -5,7 +5,11 @@ public class Main {
 //        int ans = gcdRec(27, 18);
 //        System.out.println("saving and calling it: " + ans);
 //        System.out.println("directly calling it: " + gcd(27, 18));
-        System.out.println(powO(2, 10));
+//        System.out.println(powO(2, 10));
+        int[] arr = {1, 2, 5, 12, 52, 76, 34};
+//        System.out.println(linearSearch(arr, 45));
+//        System.out.println(linearSearchRec(arr, 12, 0));
+        testRec(0);
     }
 
     public static int gcd(int a, int b) {
@@ -38,5 +42,30 @@ public class Main {
             res = res * n;
         }
         return res;
+    }
+
+    public static int linearSearch(int[] arr, int k) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == k)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int linearSearchRec(int[] arr, int k, int i) {
+        if (i == arr.length) return -1;
+        if (arr[i] == k) return i;
+        int x = linearSearchRec(arr, k, i + 1);
+        System.out.println("I'm Recursion at " + i);
+        return x;
+    }
+
+    public static void testRec(int n) {
+//        if (n == 100000) return;
+//        System.out.println(n);
+//        testRec(n + 1);
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println(i);
+        }
     }
 }
