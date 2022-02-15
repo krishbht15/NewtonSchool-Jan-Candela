03416b02148dc3ccac3f622923df8793fd8a8de5
Krish Bhatia <krishbht15>
2022-02-15 23:04:45 +0530

    15th Feb

---
+++ src/com/company/string/Main.java
@@ -0,0 +1,11 @@
+package com.company.string;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "Hey";
+        System.out.println(a);
+        System.out.println(a.length());
+        System.out.println(a.substring(1));
+    }
+}
