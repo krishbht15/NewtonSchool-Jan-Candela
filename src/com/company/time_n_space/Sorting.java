f1f1e1448fa3163348deddcd84df3cc5c4aca9bb
Krish Bhatia <krishbht15>
2022-02-17 23:04:32 +0530

    17th feb

--- src/com/company/time_n_space/Sorting.java
+++ src/com/company/time_n_space/Sorting.java
@@ -1,33 +1,19 @@
 package com.company.time_n_space;
 
+import java.util.Arrays;
+
 public class Sorting {
 
     public static void main(String[] args) {
-//        int[] arr = {5, 1, 3, 6, 10};
-//        selectionSort(arr);
-//        System.out.println(Arrays.toString(arr));
-        int n = 10;
-
-        something(n);
-        System.out.println(n);
-        StringBuilder x = new StringBuilder("I'm Old");
-        somethingStringBuilder(x);
-        System.out.println(x);
-        String a = "new snkjnak";
-        System.out.println(a.trim());
+        int[] arr = {45, 35, 25, 15, 55, 35, 75};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static void somethingString(String a) {
-        a.trim();
-    }
-
-    private static void somethingStringBuilder(StringBuilder x) {
-        x.append("now I'm Young");
-    }
-
-    public static void something(int n) {
-        n = n * 10;
-        System.out.println("newr n " + n);
+    private static void swap(int[] arr, int i, int minIdx) {
+        int temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
     }
 
     public static void selectionSort(int[] arr) {
@@ -40,9 +26,69 @@ public class Sorting {
         }
     }
 
-    private static void swap(int[] arr, int i, int minIdx) {
-        int temp = arr[i];
-        arr[i] = arr[minIdx];
-        arr[minIdx] = temp;
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (arr[j] < arr[j - 1]) swap(arr, j, j - 1);
+                else break;
+            }
+        }
+    }
+
+    //    https://leetcode.com/problems/sort-colors/
+    public static void sortColors(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int k = nums.length - 1;
+        while (i <= k) {
+            if (nums[i] == 0) {
+                swap(nums, i, j);
+                i++;
+                j++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums, i, k);
+                k--;
+            }
+        }
+    }
+//    https://leetcode.com/problems/merge-sorted-array/
+
+    public static int[] mergeTwoSortedArray(int[] n1, int[] n2) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] ans = new int[n1.length + n2.length];
+        while (i < n1.length && j < n2.length) {
+            if (n1[i] < n2[j]) {
+                ans[k] = n1[i];
+                i++;
+                k++;
+            } else {
+                ans[k] = n2[j];
+                j++;
+                k++;
+            }
+        }
+        while (i < n1.length) {
+            ans[k] = n1[i];
+            i++;
+            k++;
+        }
+        while (j < n2.length) {
+            ans[k] = n2[j];
+            j++;
+            k++;
+        }
+        return ans;
     }
 }
